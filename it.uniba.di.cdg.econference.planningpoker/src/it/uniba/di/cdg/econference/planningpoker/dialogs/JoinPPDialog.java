36f6245f74862ae677491813dcb02b0833857028
alessandro.brucoli <alessandro.brucoli@05ecb3a4-5670-11de-b59c-372aa07dcf7f>
2009-06-24 11:57:55 +0000

    Added load Planning Poker Dialog

---
+++ it.uniba.di.cdg.econference.planningpoker/src/it/uniba/di/cdg/econference/planningpoker/dialogs/JoinPPDialog.java
@@ -0,0 +1,78 @@
+package it.uniba.di.cdg.econference.planningpoker.dialogs;
+
+import static it.uniba.di.cdg.xcore.util.Misc.isEmpty;
+import it.uniba.di.cdg.xcore.econference.EConferenceContext;
+import it.uniba.di.cdg.xcore.ui.UiPlugin;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+public class JoinPPDialog extends Dialog {
+
+	
+	private LoadPPFileDialogUI ui;
+	private String fileName;
+	private String nickName;
+	private boolean sendInvitations;
+
+	public JoinPPDialog(Shell parentShell) {
+		super(parentShell);	
+	}
+	
+	
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		 Composite composite = (Composite) super.createDialogArea( parent );
+
+	        FillLayout layout = new FillLayout( SWT.VERTICAL );
+	        composite.setLayout( layout );
+	        
+	        //TODO: decidere se creare un nuovo dialogUI per il Planning Poker
+	        ui = new LoadPPFileDialogUI( composite, SWT.NONE );
+
+	        return composite;
+	}
+	
+    @Override
+    protected void okPressed() {
+        fileName = ui.getFileName();
+        nickName = ui.getNickName();
+        sendInvitations = ui.sendInvitations();
+        
+        if (validate()) {
+            getContext().setNickName( nickName );
+            super.okPressed();
+        } else {
+            UiPlugin.getUIHelper().showErrorMessage( "Please fill all required fields" );
+        }
+    }
+
+	private boolean validate() {
+        return !isEmpty( getFileName() ) && !isEmpty( getNickName() ) && getContext() != null;
+	}
+
+
+	public EConferenceContext getContext() {
+		return ui.getContext();
+	}
+
+	public boolean isSendInvitations() {		
+		return sendInvitations;
+	}
+
+
+	public String getFileName() {
+		return fileName;
+	}
+
+
+	public String getNickName() {
+		return nickName;
+	}
+	
+
+}
