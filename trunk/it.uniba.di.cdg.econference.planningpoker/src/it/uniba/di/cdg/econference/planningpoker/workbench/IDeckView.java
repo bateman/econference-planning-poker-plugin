8571e591f0cd4d2f383178c49d2fd9c5d91fa8e5
alessandro.brucoli <alessandro.brucoli@05ecb3a4-5670-11de-b59c-372aa07dcf7f>
2009-08-25 16:45:41 +0000

    Changin Views (adding start/stop button to Backlog View)

--- trunk/it.uniba.di.cdg.econference.planningpoker/src/it/uniba/di/cdg/econference/planningpoker/workbench/IDeckView.java
+++ trunk/it.uniba.di.cdg.econference.planningpoker/src/it/uniba/di/cdg/econference/planningpoker/workbench/IDeckView.java
@@ -1,10 +1,14 @@
 package it.uniba.di.cdg.econference.planningpoker.workbench;
 
-import it.uniba.di.cdg.xcore.econference.ui.views.IEConferenceView;
+import it.uniba.di.cdg.econference.planningpoker.model.deck.IPokerCard;
 import it.uniba.di.cdg.xcore.multichat.IRoleProvider;
 import it.uniba.di.cdg.xcore.ui.views.IActivatableView;
 
-public interface IDeckView extends IEConferenceView, IRoleProvider,
+public interface IDeckView extends IPlanningPokerView, IRoleProvider,
 		IActivatableView {
+
+	void setSelectedCard(IPokerCard selectedCard);
+
+	IPokerCard getSelectedCard();
 
 }
