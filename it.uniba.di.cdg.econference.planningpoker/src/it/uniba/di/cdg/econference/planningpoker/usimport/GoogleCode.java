eb3b86072dc6ec58403dd4de052c253564c70ac0
calefato.uniba@gmail.com <calefato.uniba@gmail.com@05ecb3a4-5670-11de-b59c-372aa07dcf7f>
2011-01-21 12:08:52 +0000

    fixed issue 47

--- it.uniba.di.cdg.econference.planningpoker/src/it/uniba/di/cdg/econference/planningpoker/usimport/GoogleCode.java
+++ it.uniba.di.cdg.econference.planningpoker/src/it/uniba/di/cdg/econference/planningpoker/usimport/GoogleCode.java
@@ -36,6 +36,8 @@ import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -56,79 +58,81 @@ import org.xml.sax.SAXException;
 
 public class GoogleCode {
 
-	public static LinkedList<String> note=new LinkedList<String>();
-	public static LinkedList<String> storyText=new LinkedList<String>();
-	public static LinkedList<String> id=new LinkedList<String>();
-	public static LinkedList<String> idC=new LinkedList<String>();
-	public static LinkedList<String> milestone=new LinkedList<String>();
-	public static LinkedList<String> milestoneC=new LinkedList<String>();
-	public static LinkedList<String> milestoneDesc=new LinkedList<String>();
-	public static LinkedList<String> milestoneDate=new LinkedList<String>();
-	public static LinkedList<String> milestoneTitle=new LinkedList<String>();
-	public static LinkedList<String> clearedMilestone=new LinkedList<String>();
-	public static LinkedList<String> estimates=new LinkedList<String>();
+	public static LinkedList<String> note = new LinkedList<String>();
+	public static LinkedList<String> storyText = new LinkedList<String>();
+	public static LinkedList<String> id = new LinkedList<String>();
+	public static LinkedList<String> idC = new LinkedList<String>();
+	public static LinkedList<String> milestone = new LinkedList<String>();
+	public static LinkedList<String> milestoneC = new LinkedList<String>();
+	public static LinkedList<String> milestoneDesc = new LinkedList<String>();
+	public static LinkedList<String> milestoneDate = new LinkedList<String>();
+	public static LinkedList<String> milestoneTitle = new LinkedList<String>();
+	public static LinkedList<String> clearedMilestone = new LinkedList<String>();
+	public static LinkedList<String> estimates = new LinkedList<String>();
 	private static FileReader in;
 	private static FileWriter temp;
 	private static String sub;
 	private static FileWriter csv;
 
-	public GoogleCode(String addr,String filename){
-		downloadCSV(addr); //temp.csv
+	public GoogleCode(String addr, String filename) {
+		downloadCSV(addr); // temp.csv
 		try {
-			in=new FileReader("temp.csv");
+			in = new FileReader("temp.csv");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		csv2xml(in); //temp.xml
-		File f=new File("temp.xml");
+		csv2xml(in); // temp.xml
+		File f = new File("temp.xml");
 		readFile(f);
-		retrieveStoryDescription(addr,idC);
-		CreateStandardXML c2=new CreateStandardXML(storyText,note,id,estimates,milestone,milestoneDesc,milestoneDate,milestoneTitle);
+		retrieveStoryDescription(addr, idC);
+		CreateStandardXML c2 = new CreateStandardXML(storyText, note, id,
+				estimates, milestone, milestoneDesc, milestoneDate,
+				milestoneTitle);
 		c2.saveFile(filename);
-		
+
 		// deleting temp file: temp.xml and temp.csv
 		f.delete();
-		File f2=new File("temp.csv");
+		File f2 = new File("temp.csv");
 		f2.delete();
 	}
 
-
-	public static FileWriter downloadCSV(String addr){
-		try{
-			addr=addr.replace(" ", "+");
+	public static FileWriter downloadCSV(String addr) {
+		try {
+			addr = addr.replace(" ", "+");
 			URL url = new URL(addr);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Accept", "application/csv");
 			conn.connect();
 			InputStream in = conn.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(in));
 			String text;
 			StringWriter w = new StringWriter();
-			while ((text = reader.readLine()) != null) 
-			{
+			while ((text = reader.readLine()) != null) {
 				w.write(text);
 			}
-			String s=w.toString();
-			s=s.replace(", ", "");
-			s=s.replace(",\"\",",",*,");
-			s=s.replace("\"\"", "\n");
-			s=s.replace("*", "");
-			s=s.replace("\"", "");
-			s=s.replace(",,", ",\"\",");
-			
-			csv=new FileWriter("temp.csv");
+			String s = w.toString();
+			s = s.replace(", ", "");
+			s = s.replace(",\"\",", ",*,");
+			s = s.replace("\"\"", "\n");
+			s = s.replace("*", "");
+			s = s.replace("\"", "");
+			s = s.replace(",,", ",\"\",");
+
+			csv = new FileWriter("temp.csv");
 			csv.write(s);
 			csv.close();
 			conn.disconnect();
-		}catch(IOException ex){ex.printStackTrace();}
-		
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
 		return csv;
 	}
 
+	public static FileWriter csv2xml(FileReader in) {
 
-	public static FileWriter csv2xml(FileReader in){
-	
 		BufferedReader reader = new BufferedReader(in);
 		StringBuilder xml = new StringBuilder();
 		String lineBreak = System.getProperty("line.separator");
@@ -156,7 +160,7 @@ public class GoogleCode {
 						xml.append("\t\t<");
 						xml.append(headers.get(count));
 						xml.append(">");
-						xml.append(tokenizer.nextToken());
+						xml.append(escapeXML(tokenizer.nextToken()));
 						xml.append("</");
 						xml.append(headers.get(count));
 						xml.append(">");
@@ -168,21 +172,23 @@ public class GoogleCode {
 					entryCount++;
 				}
 			}
-		} catch (IOException e) {}
+		} catch (IOException e) {
+		}
 		xml.append("</root>");
-		String text2=xml.toString().replace("\"", "");
+		String text2 = xml.toString().replace("\"", "");
 		try {
 			temp = new FileWriter("temp.xml"); // from csv2xml;
-			temp.write(text2);	 
+			temp.write(text2);
 			temp.close();
-		} catch (IOException e) {}
+		} catch (IOException e) {
+		}
 		return temp;
 	}
 
-
-	public static void readFile(File f){
-		try{
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	public static void readFile(File f) {
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory
+					.newInstance();
 			factory.setNamespaceAware(true);
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document doc = builder.parse(f);
@@ -195,9 +201,10 @@ public class GoogleCode {
 			result = expr.evaluate(doc, XPathConstants.NODESET);
 			NodeList nodeListMilestone = (NodeList) result;
 			for (int i = 0; i < nodeListMilestone.getLength(); i++) {
-				if (nodeListMilestone.item(i).getTextContent()!=null){
+				if (nodeListMilestone.item(i).getTextContent() != null) {
 					milestone.offer(nodeListMilestone.item(i).getTextContent());
-					milestoneC.offer(nodeListMilestone.item(i).getTextContent());
+					milestoneC
+							.offer(nodeListMilestone.item(i).getTextContent());
 					estimates.offer("");
 				}
 			}
@@ -206,7 +213,7 @@ public class GoogleCode {
 			result = expr.evaluate(doc, XPathConstants.NODESET);
 			NodeList nodeListSummary = (NodeList) result;
 			for (int i = 0; i < nodeListSummary.getLength(); i++) {
-				if (nodeListSummary.item(i).getTextContent()!=null){
+				if (nodeListSummary.item(i).getTextContent() != null) {
 					storyText.offer(nodeListSummary.item(i).getTextContent());
 				}
 			}
@@ -215,76 +222,101 @@ public class GoogleCode {
 			result = expr.evaluate(doc, XPathConstants.NODESET);
 			NodeList nodeListID = (NodeList) result;
 			for (int i = 0; i < nodeListID.getLength(); i++) {
-				if (nodeListID.item(i).getTextContent()!=null){
+				if (nodeListID.item(i).getTextContent() != null) {
 					id.offer(nodeListID.item(i).getTextContent());
 					idC.offer(nodeListID.item(i).getTextContent());
 				}
 			}
 
-			if (!milestoneC.isEmpty()){
-				clearedMilestone=eliminateDuplicates(milestoneC);
+			if (!milestoneC.isEmpty()) {
+				clearedMilestone = eliminateDuplicates(milestoneC);
 			}
 
+		} catch (XPathExpressionException e) {
+		} catch (ParserConfigurationException e) {
+		} catch (IOException e) {
+		} catch (SAXException e) {
 		}
-		catch (XPathExpressionException e){}
-		catch (ParserConfigurationException e){}
-		catch (IOException e){}
-		catch (SAXException e){}
 	}
 
-
-
-	public static LinkedList<String> eliminateDuplicates(LinkedList<String> lista)
-	{
-		for (int i = 0 ; i < lista.size() ; i++)
-		{
-			String curr=(String) lista.get(i);
-			for (int j=i+1 ; j< lista.size() ; j++)
-			{
-				String tmp=(String)lista.get(j);
-				if (curr.equals(tmp))
-				{
+	public static LinkedList<String> eliminateDuplicates(
+			LinkedList<String> lista) {
+		for (int i = 0; i < lista.size(); i++) {
+			String curr = (String) lista.get(i);
+			for (int j = i + 1; j < lista.size(); j++) {
+				String tmp = (String) lista.get(j);
+				if (curr.equals(tmp)) {
 					lista.remove(j);
-					j--; 
+					j--;
 				}
 			}
 		}
 		return lista;
 	}
 
-	public static void retrieveStoryDescription(String addr, LinkedList<String> idC){
-		String addrV[]=addr.split("/");
-		String pn=addrV[4];
-		try{
-			while (!idC.isEmpty()){
-				String url="http://code.google.com/p/"+pn+"/issues/detail?id="+idC.poll().toString()+"&can=1&q=type%3DStory&colspec=ID%20Type%20Summary";
+	public static void retrieveStoryDescription(String addr,
+			LinkedList<String> idC) {
+		String addrV[] = addr.split("/");
+		String pn = addrV[4];
+		try {
+			while (!idC.isEmpty()) {
+				String url = "http://code.google.com/p/" + pn
+						+ "/issues/detail?id=" + idC.poll().toString()
+						+ "&can=1&q=type%3DStory&colspec=ID%20Type%20Summary";
 				scrapeUrl(url);
 			}
-		}catch (java.lang.NullPointerException ec){}
+		} catch (java.lang.NullPointerException ec) {
+		}
 	}
 
-
-	public static void scrapeUrl(String addr){
-		try{
+	public static void scrapeUrl(String addr) {
+		try {
 			URL url = new URL(addr);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.connect();
 			InputStream in = conn.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(in));
 			String text;
 			StringWriter w = new StringWriter();
-			while ((text = reader.readLine()) != null) 
-			{
+			while ((text = reader.readLine()) != null) {
 				w.write(text);
 			}
 			int pos = w.toString().indexOf("<div class=\"author\">");
 			String parte = w.toString().substring(pos);
-			pos=parte.indexOf("</div>");
-			int pos2=parte.indexOf("</td>");
-			sub = parte.substring(pos+11, pos2-7);
+			pos = parte.indexOf("</div>");
+			int pos2 = parte.indexOf("</td>");
+			sub = parte.substring(pos + 11, pos2 - 7);
 			note.offer(sub.toString());
 			conn.disconnect();
-		}catch(IOException ex){}
+		} catch (IOException ex) {
+		}
+	}
+
+	private static String escapeXML(String aText) {
+		final StringBuilder result = new StringBuilder();
+		final StringCharacterIterator iterator = new StringCharacterIterator(
+				aText);
+		char character = iterator.current();
+		while (character != CharacterIterator.DONE) {
+			if (character == '<') {
+				result.append("&lt;");
+			} else if (character == '>') {
+				result.append("&gt;");
+			} else if (character == '\"') {
+				result.append("&quot;");
+			} else if (character == '\'') {
+				result.append("&#039;");
+			} else if (character == '&') {
+				result.append("&amp;");
+			} else {
+				// the char is not a special one
+				// add it to the result as is
+				result.append(character);
+			}
+			character = iterator.next();
+		}
+		return result.toString();
 	}
 }
