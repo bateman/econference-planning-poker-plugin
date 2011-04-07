1957e16d3a46c5cd7f538f44ee16b0b5917ea312
calefato.uniba@gmail.com <calefato.uniba@gmail.com@05ecb3a4-5670-11de-b59c-372aa07dcf7f>
2011-04-07 15:18:35 +0000

    * finished migration of eCP3 to the use of new aspects bundle

--- it.uniba.di.cdg.econference.planningpoker/src/it/uniba/di/cdg/econference/planningpoker/model/PlanningPokerModel.java
+++ it.uniba.di.cdg.econference.planningpoker/src/it/uniba/di/cdg/econference/planningpoker/model/PlanningPokerModel.java
@@ -25,7 +25,8 @@
  */
 package it.uniba.di.cdg.econference.planningpoker.model;
 
-import it.uniba.di.cdg.aspects.ThreadSafetyAspect;
+import it.uniba.di.cdg.aspects.GetSafety;
+import it.uniba.di.cdg.aspects.SetSafety;
 import it.uniba.di.cdg.econference.planningpoker.model.backlog.Backlog;
 import it.uniba.di.cdg.econference.planningpoker.model.deck.CardDeck;
 import it.uniba.di.cdg.econference.planningpoker.model.estimates.IEstimatesList;
@@ -33,123 +34,109 @@ import it.uniba.di.cdg.econference.planningpoker.model.estimates.Voters;
 import it.uniba.di.cdg.xcore.econference.model.ConferenceModel;
 import it.uniba.di.cdg.xcore.m2m.model.IChatRoomModelListener;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+public class PlanningPokerModel extends ConferenceModel implements
+		IPlanningPokerModel {
 
-public class PlanningPokerModel extends ConferenceModel implements IPlanningPokerModel {
-	
 	/**
 	 * The list of user stories in the backlog
 	 */
 	private Backlog backlog;
-	
+
 	private CardDeck deck;
-	
-	
+
 	private IEstimatesList estimates;
-	
-	private Voters voters; 
-	
 
-	 public PlanningPokerModel() {
-	        super();
-	        new DefaultModelFactory();	
-	        
-	        this.deck = new CardDeck();
-	        this.backlog = new Backlog();
-	        
-	        this.voters = new Voters();	        
-	    }
+	private Voters voters;
 
-	
+	public PlanningPokerModel() {
+		super();
+		new DefaultModelFactory();
+
+		this.deck = new CardDeck();
+		this.backlog = new Backlog();
+
+		this.voters = new Voters();
+	}
+
 	@Override
+	@SetSafety
 	public void setBacklog(Backlog backlog) {
-//		if(this.backlog!=null)
-//			this.backlog.dispose();
-//		this.backlog = backlog;
-//		for (IChatRoomModelListener l : listeners()) {
-//            if (l instanceof IPlanningPokerModelListener) {
-//                ((IPlanningPokerModelListener) l).backlogChanged();
-//            }
-//        }
-		/* We do not set the backlog to the new object to avoid 
-		 * adding the backlogChanged method listener in all views 
+		// if(this.backlog!=null)
+		// this.backlog.dispose();
+		// this.backlog = backlog;
+		// for (IChatRoomModelListener l : listeners()) {
+		// if (l instanceof IPlanningPokerModelListener) {
+		// ((IPlanningPokerModelListener) l).backlogChanged();
+		// }
+		// }
+		/*
+		 * We do not set the backlog to the new object to avoid adding the
+		 * backlogChanged method listener in all views
 		 */
-		this.backlog.setBacklogContent(backlog.getUserStories());	
+		this.backlog.setBacklogContent(backlog.getUserStories());
 		this.backlog.setCurrentItemIndex(backlog.getCurrentItemIndex());
 	}
 
+	@GetSafety
 	@Override
 	public Backlog getBacklog() {
 		return backlog;
 	}
 
+	@GetSafety
 	@Override
 	public CardDeck getCardDeck() {
 		return deck;
 	}
 
+	@SetSafety
 	@Override
 	public void setCardDeck(CardDeck deck) {
-		if(this.deck!=null)
+		if (this.deck != null)
 			this.deck.dispose();
-		this.deck = deck;	
+		this.deck = deck;
 		for (IChatRoomModelListener l : listeners()) {
-            if (l instanceof IPlanningPokerModelListener) {
-                ((IPlanningPokerModelListener) l).cardDeckChanged();
-            }
-        }
+			if (l instanceof IPlanningPokerModelListener) {
+				((IPlanningPokerModelListener) l).cardDeckChanged();
+			}
+		}
 	}
 
-
+	@GetSafety
 	@Override
-	public IEstimatesList getEstimateSession() {		
+	public IEstimatesList getEstimateSession() {
 		return estimates;
 	}
 
-
 	@Override
 	public void openEstimateSession(IEstimatesList estimates) {
-		if(this.estimates!=null)
+		if (this.estimates != null)
 			this.estimates.dispose();
 		this.estimates = estimates;
 		for (IChatRoomModelListener l : listeners()) {
-            if (l instanceof IPlanningPokerModelListener) {
-                ((IPlanningPokerModelListener) l).estimateSessionOpened();
-            }
-        }
+			if (l instanceof IPlanningPokerModelListener) {
+				((IPlanningPokerModelListener) l).estimateSessionOpened();
+			}
+		}
 	}
-	
+
+	@SetSafety
 	@Override
 	public void setVoters(Voters voters) {
-		if(this.voters!=null)
+		if (this.voters != null)
 			this.voters.dispose();
 		this.voters = voters;
 		for (IChatRoomModelListener l : listeners()) {
-            if (l instanceof IPlanningPokerModelListener) {
-                ((IPlanningPokerModelListener) l).votersListChanged();
-            }
-        }
+			if (l instanceof IPlanningPokerModelListener) {
+				((IPlanningPokerModelListener) l).votersListChanged();
+			}
+		}
 	}
-	
+
+	@GetSafety
 	@Override
-	public Voters getVoters(){
+	public Voters getVoters() {
 		return voters;
-	}
-
-	/**
-	 * Provides internal thread synchronization.
-	 */
-	@Aspect
-	public static class OwnThreadSafety extends ThreadSafetyAspect {
-
-		@Pointcut("execution( public * PlanningPokerModel.get*(..) )")
-		protected void readOperations() {
-		}
-
-		@Pointcut("execution( public void PlanningPokerModel.set*(..) )")
-		protected void writeOperations() {
-		}
 	}
 
 }
